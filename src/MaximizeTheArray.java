917d1f77e67d14d9982bf5e3fdef75aac837d2e8
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-27 00:59:30 -0700

    coded the method to return the maximum array(which means the array containing larger numbers from arr1 and arr2,but no duplicates and the order should be arr2 first and then arr1

--- src/MaximizeTheArray.java
+++ src/MaximizeTheArray.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 public class MaximizeTheArray {
-    
+    public static ArrayList<Integer> maximizeTheArray(int [] arr1, int[] arr2, int n){
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        for(int i=0; i<n; i++){
+            ts.add(arr1[i]);
+        }
+        for(int i=0; i<n; i++){
+            ts.add(arr1[i]);
+        }
+        while(ts.size()> n){
+            ts.remove(ts.first());
+        }
+        for(int i=0; i<n; i++){
+            if(ts.contains(arr2[i])){
+                ans.add(arr2[i]);
+                ts.remove(arr2[i]);
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(ts.contains(arr1[i])){
+                ans.add(arr1[i]);
+                ts.remove(arr1[i]);
+            }
+        }return ans;
+
+
+    }
     public static void main(String[] args) {
 
     }
